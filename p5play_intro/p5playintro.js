f7507448c1f656c3347f21ff44f5d0bfbf46859c
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-06 10:24:11 +0000

    modified:   p5play_intro/p5playintro.js

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,16 +1,20 @@
+let ball;
+let box;
 function setup() {
   // Set up the canvas
   new Canvas(800, 400);
   background(250); //background color
-
-  // Basic shape testing
-  // write your codes here
- 
-  // End Basic shape testing
-
-   // Create a bouncing ball sprite
-   // write your codes here
-
+  ball = new Sprite();
+  ball.x = 100
+  ball.y = 200;
+  ball.diameter = 40;
+  ball.colour = 'blue';
+  box = new Sprite();
+  box.x = 100;
+  box.y = 100;
+  box.w = 50;
+  box.h = 50;
+  box.colour = "green"
 }
 
 function draw() {
