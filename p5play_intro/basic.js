5102db1cb8b1b981e127301bca1094cf6f048fae
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-13 10:30:50 +0000

    Jul 13, 2026, 10:30 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -21,7 +21,7 @@ function setup() {
   // --- Exercise: Age category classification ---
   let age = 9
   if (age <= 9){
-    
+    console.log("")
   }
 
   // --- Exercise: Display odd numbers backward using while loop ---
