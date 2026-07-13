345d99b75b83c6ed1ab7731ef648939a2cb8bc78
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-13 10:32:52 +0000

    Jul 13, 2026, 10:32 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -22,7 +22,7 @@ function setup() {
   let age = 9
   if (age <= 9){
     console.log("Lower priary");
-  }else if (age <= 12){console.log("Upper priary");}else(){}
+  }else if (age <= 12){console.log("Upper priary");}else{console.log("lower priary");}
 
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
