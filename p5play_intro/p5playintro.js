ce91d62b53dc46c68e51b3c0b624e04c9efaa44d
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-13 09:20:31 +0000

    Jul 13, 2026, 9:20 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -18,5 +18,29 @@ function setup() {
 }
 
 function draw() {
-  // write your codes here
+    // what happens if you don't have this line? try it.
+    background(240); // clear screen each frame
+
+    // Showing coordinates for sprites
+    fill(0);
+    textSize(16);
+    text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10, 20);
+    text("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 40);
+
+    // Animate ball by updating position automatically via velocity
+    // Bounce off edges using manual wall check
+
+    // if ball x position goes off the left wall or right wall
+    if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
+        ball.vel.x *= -1; // change +x to -x or vice versa
+    }
+
+    // if ball y position goes off the top or bottom
+    if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
+        ball.vel.y *= -1; // change +y to -y or vice versa
+    }
+
+    // Make follower follow the mouse
+    box.x = mouseX;
+    box.y = mouseY;
 }
