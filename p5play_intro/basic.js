3354d1fea11d06719a5daedbd68a23151f4720c9
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-13 10:00:33 +0000

    Jul 13, 2026, 10:00 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -3,7 +3,7 @@ function setup() {
   new Canvas(800, 400);
   console.log("Hello");
   console.log(area)
-  text(area)
+  Text(area)
   // --- Exercise: Area of Triangle ---
   // write your codes here
 
