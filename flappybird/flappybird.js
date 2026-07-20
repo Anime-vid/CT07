eefe8255b0452e8be5a70cec9b6c488428444151
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-20 10:31:04 +0000

    Jul 20, 2026, 10:31 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -1,1 +1,9 @@
-// the final game
+let bird, floor; // Declaring variables for objects
+let flapMidImg, bg, base; // Declaring variables for images
+
+function preload() {
+    // bird image, background and the floor
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+}
