bd6d85ff50c1e455b05794227b7d39ed2fb39010
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-27 10:17:37 +0000

    Jul 27, 2026, 10:17 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -55,7 +55,7 @@ function setup() {
   text('sleeping: ' + bird.sleeping , 10, 60);
   if (bird.vel.y < 0){
     bird.img = downFlapImg;
-    bird.ro
+    bird.rotation = 90
   } else if (bird.vel.y > 0){
     bird.img = upFlapImg;
   }else{bird.img = flapMidImg;
