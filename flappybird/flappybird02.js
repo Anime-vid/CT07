09819de530221d048fb193bd1ee9e4fda2168c11
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-27 10:10:45 +0000

    Jul 27, 2026, 10:10 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -6,6 +6,11 @@ function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    downFlapImg = loadImage('assets/yellowbird-downflap.png');
+    upFlapImg = loadImage('assets/yellowbird-upflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
 }
 
 function setup() {
