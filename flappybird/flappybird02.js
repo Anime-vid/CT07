ce12a1f2f6eb8b82d104c381e1fe9ec79598a0e2
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-27 10:36:11 +0000

    Jul 27, 2026, 10:36 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -83,4 +83,5 @@ function setup() {
     bottomPipe = new Sprite(400,midY + gap / 2 + 200,52,320,'static');
     bottomPipe.img = pipe;
     pipeGroup.add(bottomPipe);
+    pipeGroup.layer = 0;
   }
