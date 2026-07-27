6ee8b87eb8a801571399778be1c0500779ce77d8
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-07-27 10:20:00 +0000

    Jul 27, 2026, 10:20 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -27,7 +27,7 @@ function setup() {
   bird.collider = "dynamic";
   bird.mass = 10;
   bird.drag = 0.02;
-  bird.bounciness = 0;
+  bird.bounciness = -100;
   world.gravity.y = 10;
 
   // Floor to bounce bird
