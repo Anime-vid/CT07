050d1500037b4e6141fac445d1755457155d6d35
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-03 10:33:26 +0000

    Aug 3, 2026, 10:33 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -53,13 +53,6 @@ function setup() {
  function draw() {
   if (bird.collides(pipeGroup) || bird.collides(floor))
   {
-
-
-
-
-
-
-
     gameoverLabel = new Sprite(width/2,height/2,192,42);
     gameoverLabel.img = gameoverImg;
     gameoverLabel.layer = 100
