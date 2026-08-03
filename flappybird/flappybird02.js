c87e37b2ca2e135d4ca148860da1c0717f5e73c6
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-03 10:05:02 +0000

    Aug 3, 2026, 10:05 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -84,7 +84,7 @@ function setup() {
 
 function spawnPipePair(){
   let gap = 50;
-  let midY = random;
+  let midY = random(250,height);
   let spawnX = bird.x + 400; // spawn off-screen ahead of the bird, not at a fixed x
 
   bottomPipe = new Sprite(spawnX, midY + gap / 2 + 200, 52, 320, 'static');
