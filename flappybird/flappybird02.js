70db2049cf6b6b8ff07e32bc590055ed06aeb7f0
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-03 10:38:02 +0000

    Aug 3, 2026, 10:38 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -5,6 +5,7 @@ let bottomPipe;
 let topPipe;
 let startScreenLabel;
 let startScreenImg;
+let 
 
 function preload() {
     // bird image, background and the floor
@@ -17,7 +18,7 @@ function preload() {
     base = loadImage('assets/base.png');
     pipe = loadImage('assets/pipe-green.png')
     gameoverImg = loadImage('assets/gameover.png')
-    startScreenImg = loadImage(assets/message.png)
+    startScreenImg = loadImage('assets/message.png')
 }
 
 function setup() {
