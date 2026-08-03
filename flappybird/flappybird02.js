a7b736237cd4e6087adc2e209713cf5bb06a8a65
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-03 10:11:30 +0000

    Aug 3, 2026, 10:11 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -45,7 +45,7 @@ function setup() {
   pipeGroup = new Group();
 }
  function draw() {
-  if (bird.collides(pipeGroup) )
+  if (bird.collides(pipeGroup) || bird.collides(floor))
   image(bg, 0, 0, width, height);
 
    // Apply upward push when space is pressed
