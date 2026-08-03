3eb5129eb944cbbea7481378450997bc42d4b0e9
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-03 10:31:43 +0000

    Aug 3, 2026, 10:31 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -47,7 +47,8 @@ function setup() {
   floor.collider = "static";
   floor.img = base;
   pipeGroup = new Group();
-  startScreenLabel = new Sprite(width/2,height/2)
+  startScreenLabel = new Sprite(width/2,height/2,50,50,'none');
+  st
 }
  function draw() {
   if (bird.collides(pipeGroup) || bird.collides(floor))
