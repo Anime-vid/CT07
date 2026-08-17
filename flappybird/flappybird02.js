64ca9002cb69b224f6a3cc24d245230f9446d156
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-17 09:48:53 +0000

    Aug 17, 2026, 9:48 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -123,5 +123,6 @@ function spawnPipePair(){
 }
 function drawScore(x,y,score,digitWidth,digitHeight) {
   scoreDigits.removeAll();
-  let scoreStr = str(score)
+  let scoreStr = str(score);
+  let total
 }
