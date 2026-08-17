f9b3f121bd90b7aab672d00a2d5fe2b4a3889fe7
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-17 10:21:59 +0000

    Aug 17, 2026, 10:21 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -153,6 +153,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
   }
 }
 function moveGroup(group,targetX,spacing) {
-  let totalWidth = (group.length -1)
+  let totalWidth = (group.length -1) * spacing;
+  let startX = ()
 
 }
