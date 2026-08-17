a774a7cfb754b6bfbf42d2fb68f00443ade15dc3
marcel sim <146540998+Anime-vid@users.noreply.github.com>
2026-08-17 10:24:52 +0000

    Aug 17, 2026, 10:24 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -140,7 +140,6 @@ function spawnPipePair() {
 }
 
 function drawScore(x, y, score, digitWidth, digitHeight) {
-  moveGroup(scoreDigits,camera.x,24);
   scoreDigits.removeAll();
   let scoreStr = str(score);
   let totalWidth = scoreStr.length * digitWidth;
@@ -149,7 +148,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
     let digit = int(scoreStr[i]);
     let xPos = startX + i * digitWidth;
     let digitSprite = new scoreDigits.Sprite(xPos, y, digitWidth, digitHeight);
-    digitSprite.img = numberImages[digit];
+    digitSprite.img = numberImages[digit]
   }
 }
 function moveGroup(group,targetX,spacing) {
